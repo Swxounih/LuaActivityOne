95545ee6e5c4c737187292ccf9c764eba19c04c9
Armel Cruz <155382655+Swxounih@users.noreply.github.com>
2026-02-23 20:48:53 +0800

    Secret Code ng Commit na to
    di dapat ganito maglagay ng summary per o keri na yan since nag aaral pa lang naman tayo ng git and github Mwehehe

--- main.lua
+++ main.lua
@@ -5,3 +5,8 @@ local composer = require ("composer")
 
 --end
 
+-- asmir ito yung commit ko kapag kinommit ko to sa github makikita mo sa local device mo kapag nag pull origin ka 
+-- ta's makikita din nila Jaja, Josh,Ian  kung sino yung gumawa ng commit na to, at kung anong oras ginawa yung commit na to, at kung anong mga files yung binago sa commit na to
+-- ta's makikita din nila yung mga changes na ginawa ko sa code, at kung anong mga lines yung binago ko sa code, at kung anong mga lines yung nadagdag ko sa code, at kung anong mga lines yung nabura ko sa code
+-- kapag nakikita niyo to ibig-sabihin may chika ako bukas 
+-- para maconfirm na nakikita niyo to chat kayo sa gc ng "Fries" "Milktea" "Borger" kahit ano diyan HAHAHAHA 
